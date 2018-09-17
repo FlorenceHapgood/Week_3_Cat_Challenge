2eac81c64bed0faa7128adcba05835e0eb1fc457
Florence Hapgood <flohapgood@icloud.com>
2018-09-17 17:39:15 +0100

    Got params working_Darcie_is_leaving_so_flaky

--- app.rb
+++ app.rb
@@ -13,6 +13,13 @@ get '/dogs' do
   'Sheepdog.'
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
